d64bdac6bf7b6b52576d61b34e765984dd710435
Darkness4 <nguyen_marc@live.fr>
2019-03-23 01:58:31 +0100

    goddamn fix script

--- files/script.sh
+++ files/script.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 ANDROID_COMPILE_SDK=28
 ANDROID_BUILD_TOOLS=28.0.3
 ANDROID_SDK_TOOLS=4333796
 FLUTTER_CHANNEL=master
+INSTALL_DIR=$HOME
+
+PATH=$INSTALL_DIR/android-sdk/tools/bin:$INSTALL_DIR/android-sdk/platform-tools:$INSTALL_DIR/flutter/bin:$INSTALL_DIR/flutter/bin/cache/dart-sdk/bin:$PATH
 
 sudo apt-get update \
     && apt-get -y install --no-install-recommends apt-transport-https curl git openjdk-8-jdk lib32stdc++6 unzip \
@@ -11,28 +14,29 @@ sudo apt-get update \
     && apt-get autoclean \
     && rm -rf /var/lib/apt/lists/*
 
-git clone -b "${FLUTTER_CHANNEL}" https://github.com/flutter/flutter.git /opt/flutter
+sudo git clone -b "${FLUTTER_CHANNEL}" https://github.com/flutter/flutter.git "$INSTALL_DIR/flutter"
 
-curl -O "https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip" \
-    && mkdir /opt/android-sdk \
-    && unzip -qq "sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip" -d /opt/android-sdk \
-    && rm "sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip"
+curl -O "https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip"
+mkdir /opt/android-sdk
+unzip -qq "sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip" -d "$INSTALL_DIR/android-sdk"
 
-mkdir ~/.android \
-    && echo 'count=0' > ~/.android/repositories.cfg \
-    && yes | sdkmanager --licenses \
-    && sdkmanager "tools" \
-    "build-tools;${ANDROID_BUILD_TOOLS}" \
-    "platforms;android-${ANDROID_COMPILE_SDK}" \
-    "platform-tools" > /dev/null \
-    && yes | sdkmanager --licenses || echo "Failed" \
-    && flutter doctor -v
+mkdir ~/.android
+echo 'count=0' > ~/.android/repositories.cfg
+yes | sdkmanager --licenses
+sdkmanager "tools" "build-tools;${ANDROID_BUILD_TOOLS}" "platforms;android-${ANDROID_COMPILE_SDK}" "platform-tools"
+yes | sdkmanager --licenses || echo "Failed"
+flutter doctor -v
+
+
+if [[ ! -e ~/.profile.bak ]]; then
+    cp ~/.profile ~/.profile.bak
+fi
 
 {
-  echo "export PATH=/opt/android-sdk/tools/bin:/opt/android-sdk/platform-tools:/opt/flutter/bin:/opt/flutter/bin/cache/dart-sdk/bin:\$PATH"
-  echo "export ANDROID_HOME = /opt/android-sdk"
-  echo "export ANDROID_SDK_ROOT = /opt/android-sdk"
-  echo "export ANDROID_SDK_HOME = \$HOME"
-  echo "export JAVA_HOME = /usr/lib/jvm/java-8-openjdk-amd64"
-  echo "export PATH = \$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin"
+    echo "export PATH=$INSTALL_DIR/android-sdk/tools/bin:$INSTALL_DIR/android-sdk/platform-tools:$INSTALL_DIR/flutter/bin:$INSTALL_DIR/flutter/bin/cache/dart-sdk/bin:\$PATH"
+    echo "export ANDROID_HOME=$INSTALL_DIR/android-sdk"
+    echo "export ANDROID_SDK_ROOT=$INSTALL_DIR/android-sdk"
+    echo "export ANDROID_SDK_HOME=\$HOME"
+    echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"
+    echo "export PATH=\$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin"
 } >> ~/.profile
--- files/script_windows_ADMIN.ps1
+++ files/script_windows_ADMIN.ps1
@@ -3,14 +3,7 @@ Set-Variable ANDROID_BUILD_TOOLS 28.0.3
 Set-Variable ANDROID_SDK_TOOLS 4333796
 Set-Variable FLUTTER_CHANNEL master
 Set-Variable DISQUE C:
-
-[System.Environment]::SetEnvironmentVariable($env:Path, "$env:Path;C:\Program Files\Java\jdk1.8.0_201\bin;$DISQUE\android-sdk\tools\bin;$DISQUE\android-sdk\platform-tools;$DISQUE/flutter/bin;$DISQUE\flutter\bin\cache\dart-sdk\bin", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("ANDROID_HOME", "$DISQUE\android-sdk", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("ANDROID_SDK_ROOT", "$DISQUE\android-sdk", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("ANDROID_SDK_HOME", "\%HOME%", [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk1.8.0_201", [System.EnvironmentVariableTarget]::Machine)
-
-refreshenv
+$env:Path += ";$env:Path;C:\Program Files\Java\jdk1.8.0_201\bin;$DISQUE\android-sdk\tools\bin;$DISQUE\android-sdk\platform-tools;$DISQUE/flutter/bin;$DISQUE\flutter\bin\cache\dart-sdk\bin"
 
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -22,7 +15,6 @@ git clone -b "${FLUTTER_CHANNEL}" https://github.com/flutter/flutter.git $DISQUE
 Invoke-WebRequest "https://dl.google.com/android/repository/sdk-tools-windows-${ANDROID_SDK_TOOLS}.zip" -OutFile out.zip
 mkdir $DISQUE\android-sdk
 Expand-Archive out.zip -DestinationPath $DISQUE\android-sdk
-Remove-Item out.zip
 
 mkdir ~/.android
 Write-Output 'count=0' > ~/.android/repositories.cfg
@@ -30,4 +22,12 @@ sdkmanager --licenses
 sdkmanager "tools" "build-tools;${ANDROID_BUILD_TOOLS}" "platforms;android-${ANDROID_COMPILE_SDK}" "platform-tools"
 sdkmanager --licenses
 flutter doctor -v
+
+[System.Environment]::SetEnvironmentVariable($env:Path, "$env:Path;C:\Program Files\Java\jdk1.8.0_201\bin;$DISQUE\android-sdk\tools\bin;$DISQUE\android-sdk\platform-tools;$DISQUE/flutter/bin;$DISQUE\flutter\bin\cache\dart-sdk\bin", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk1.8.0_201", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("ANDROID_HOME", "$DISQUE\android-sdk", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("ANDROID_SDK_ROOT", "$DISQUE\android-sdk", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("ANDROID_SDK_HOME", "%HOME%", [System.EnvironmentVariableTarget]::Machine)
+
+refreshenv
 
